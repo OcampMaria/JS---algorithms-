88643c708fe44634ca519971282a2fd43a862d6c
Ocampo <mariaocampo0513@gmail.com>
2022-05-31 22:04:38 -0700

    Added simple algorithm

---
+++ script.js
@@ -0,0 +1,15 @@
+// crea algoritmo que solicite al usuario uno o mas valores ingresados por prompt(), compare las entradas y , en funcion de ciertas condiciones, muestre por consola o alert () el resultado segun los valores ingresados y las condiciones cumplidas
+
+// pedir un numero mediante prompt y si es mayor a 1000 mostrar un alert 
+let numeroIngresado = prompt("ingresar numero"); 
+
+if (numeroIngresado <= 1000) {
+    alert ("El numero ingresado muy pequeno")
+} else {
+    alert ("El numero ingresado mayor a 1000!!")
+}
+
+//  pedir un texto mediante prompt y si es igual a "hola" mostrar un alerta o consola
+
+// pedir un  numero por pormpt y evaluar si esta entre 10 y 50. si en caso positivo, mostrar un alert. 
+
